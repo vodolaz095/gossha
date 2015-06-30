ee8f2d91efeb2bb54fbb5dbb603999bf4b969d89
Ostroumov Anatolij <ostroumov095@gmail.com>
2015-06-30 05:10:30 +0300

    unit test for usage of MySQL and PostgreSQL databases

--- handler_test.go
+++ handler_test.go
@@ -1,10 +1,9 @@
 package gossha
 
 import (
-	//	"fmt"
 	"net"
+	"os"
 	"testing"
-	//	"time"
 )
 
 type connFakeAddr struct{}
@@ -37,7 +36,7 @@ func (c connMetadataFake) RemoteAddr() net.Addr {
 	return connFakeAddr{}
 }
 
-func TestInitDatabase(t *testing.T) {
+func TestSqlite3InitDatabase(t *testing.T) {
 	err := InitDatabase("sqlite3", ":memory:")
 	if err != nil {
 		t.Error(err.Error())
@@ -48,7 +47,7 @@ func TestInitDatabase(t *testing.T) {
 	}
 }
 
-func TestLoginByUsernameAndPassword(t *testing.T) {
+func TestSqlite3LoginByUsernameAndPassword(t *testing.T) {
 	h1 := New()
 	m := connMetadataFake{}
 	err := h1.LoginByUsernameAndPassword(m, "testPassword")
@@ -76,5 +75,97 @@ func TestLoginByUsernameAndPassword(t *testing.T) {
 	}
 	if h2.CurrentUser.Name != "" {
 		t.Error("We have authorized wrong user via Handler.LoginByUsernameAndPassword!")
+	}
+}
+
+func TestMysqlInitDatabase(t *testing.T) {
+	if os.Getenv("IS_TRAVIS") == "YES" {
+		err := InitDatabase("mysql", "travis:@/gossha_test?charset=utf8")
+		if err != nil {
+			t.Error(err.Error())
+		}
+		err = CreateUser("testUser", "testPassword", false)
+		if err != nil {
+			t.Error(err.Error())
+		}
+	}
+}
+
+func TestMysqlLoginByUsernameAndPassword(t *testing.T) {
+	if os.Getenv("IS_TRAVIS") == "YES" {
+		h1 := New()
+		m := connMetadataFake{}
+		err := h1.LoginByUsernameAndPassword(m, "testPassword")
+		if err != nil {
+			t.Error(err.Error())
+		}
+		if h1.CurrentUser.Name != "testUser" {
+			t.Error("We have authorized wrong user via Handler.LoginByUsernameAndPassword!")
+		}
+		if h1.SessionId != "lalalalala" {
+			t.Error("Session Id is not set via Handler.LoginByUsernameAndPassword!")
+		}
+		if h1.Ip != "127.0.0.1" {
+			t.Error("Remote IP  is not set via Handler.LoginByUsernameAndPassword!")
+		}
+
+		h2 := New()
+		err = h2.LoginByUsernameAndPassword(m, "wrongTestPassword")
+		if err != nil {
+			if err.Error() != "Wrong password for user testUser!" {
+				t.Error(err.Error())
+			}
+		} else {
+			t.Error("No error for wrong password via Handler.LoginByUsernameAndPassword!")
+		}
+		if h2.CurrentUser.Name != "" {
+			t.Error("We have authorized wrong user via Handler.LoginByUsernameAndPassword!")
+		}
+	}
+}
+
+func TestPgInitDatabase(t *testing.T) {
+	if os.Getenv("IS_TRAVIS") == "YES" {
+		err := InitDatabase("postgres", "postgres://postgres:@localhost/gossha_test")
+		if err != nil {
+			t.Error(err.Error())
+		}
+		err = CreateUser("testUser", "testPassword", false)
+		if err != nil {
+			t.Error(err.Error())
+		}
+	}
+}
+
+func TestPgLoginByUsernameAndPassword(t *testing.T) {
+	if os.Getenv("IS_TRAVIS") == "YES" {
+		h1 := New()
+		m := connMetadataFake{}
+		err := h1.LoginByUsernameAndPassword(m, "testPassword")
+		if err != nil {
+			t.Error(err.Error())
+		}
+		if h1.CurrentUser.Name != "testUser" {
+			t.Error("We have authorized wrong user via Handler.LoginByUsernameAndPassword!")
+		}
+		if h1.SessionId != "lalalalala" {
+			t.Error("Session Id is not set via Handler.LoginByUsernameAndPassword!")
+		}
+		if h1.Ip != "127.0.0.1" {
+			t.Error("Remote IP  is not set via Handler.LoginByUsernameAndPassword!")
+		}
+
+		h2 := New()
+		err = h2.LoginByUsernameAndPassword(m, "wrongTestPassword")
+		if err != nil {
+			if err.Error() != "Wrong password for user testUser!" {
+				t.Error(err.Error())
+			}
+		} else {
+			t.Error("No error for wrong password via Handler.LoginByUsernameAndPassword!")
+		}
+		if h2.CurrentUser.Name != "" {
+			t.Error("We have authorized wrong user via Handler.LoginByUsernameAndPassword!")
+		}
 	}
 }
